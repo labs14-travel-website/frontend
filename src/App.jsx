e09ed4ae4fac0e7f0111d4cb88e7004b61bbe4b2
Your Name <xcthaeh@gmail.com>
2019-07-11 17:28:28 -0400

    code along

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import Users from './Users';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Users from "./Users";
+import logo from "./logo.svg";
+import GoogleLogin from "react-google-login";
+import "./App.css";
 
 function App() {
+  const responseGoogle = res => console.log(res);
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +23,13 @@ function App() {
         </a>
       </header>
       <Users />
+      <GoogleLogin
+        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+      />
     </div>
   );
 }
