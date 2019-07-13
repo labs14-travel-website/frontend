fe03c56a5375514b95d157228da5c8b737f99ecf
Arron M <marshall.arron@gmail.com>
2019-07-13 14:08:51 -0700

    Add google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
-import Users from "./Users";
-import axios from "axios";
 import { GoogleLogin, GoogleLogout } from "react-google-login";
+import ga from 'react-ga';
+import axios from "axios";
+
+import Users from "./Users";
 import store from "./utils/jwt-store";
 import style from "./App.module.scss";
 
@@ -19,6 +21,16 @@ function App() {
         loggedIn: true
       }));
     }
+    
+    // DISCUSSION: Should this be moved to env vars? E.g. ga.initialize(process.env.GA_ID)
+    /*
+      Alternatively we could just run in production--if there is no benefit to getting analytics on staging
+      if (process.env.DB_ENV === 'production') {
+        ga.initialize('UA-143824465-1');
+      }
+    */
+    ga.initialize('UA-143824465-1', { debug: true });
+    ga.pageview('/');
   }, []);
 
   const responseGoogle = res => {
