cbd8d4c51727bc44fc3521991731d879866f8d59
josh182014 <josh182014@gmail.com>
2019-07-23 16:41:23 -0600

    added popular destinations components

---
+++ src/components/PopularDestinations.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PopularDestinations = (props) => {
+    return (
+        <div>
+            <h2>Popular Destinations!</h2>
+        </div>
+    );
+}
+ 
+export default PopularDestinations;
