1c101ef90e6d88a23923db064a5be4f94a7af817
Jamie Hall <hall.jamie@gmail.com>
2019-07-23 19:37:59 -0400

    Set up scaffolding

---
+++ src/components/CardDisplay/CardDisplay.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const CardDisplay = () => <h1>Test</h1>;
+
+export default CardDisplay;
---
+++ src/components/CardDisplay/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import CardDisplay from './CardDisplay';
+
+
+const index = () => <CardDisplay />;
+export default index;
