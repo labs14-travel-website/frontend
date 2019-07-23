5d34739e23c21646b125d4fa8333572b8013062f
josh182014 <josh182014@gmail.com>
2019-07-23 16:48:58 -0600

    Fixed linting

--- src/components/PopularDestinations.jsx
+++ src/components/PopularDestinations.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const PopularDestinations = (props) => {
-    return (
-        <div>
-            <h2>Popular Destinations!</h2>
-        </div>
-    );
-}
- 
+const PopularDestinations = () => (
+  <div>
+    <h2>Popular Destinations!</h2>
+  </div>
+);
+
 export default PopularDestinations;
