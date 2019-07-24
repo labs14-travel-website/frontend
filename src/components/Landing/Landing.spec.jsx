f0189cd27f417069e22b9852e455a59b00d0666f
azinoVo <azinovo22@gmail.com>
2019-07-24 17:56:15 -0500

    added axios req and general unit tests using jest-enzyme

--- src/components/Landing/Landing.spec.jsx
+++ src/components/Landing/Landing.spec.jsx
@@ -1,3 +1,47 @@
+import React from 'react';
+import { shallow, mount, render } from '../../enzyme.js';
+import Landing from './Landing.jsx';
+
 it('tests true', () => {
   expect(1).toBe(1);
 });
+
+describe('Search Bar', () => {
+  it('Search Bar rendered', () => {
+    const component = shallow(<Landing />);
+    expect(component.find('.search').exists()).toBe(true);
+  })
+
+  it('Search should mount once', function () {
+    expect(mount(<Landing />).find('.search').length).toBe(1);
+  });
+
+  it('Search Bar text', () => {
+    const component = shallow(<Landing />);
+    expect(component.find('[className="search"]').text()).toEqual('Roam');
+  })
+
+  it('Search Bar Simulation', () => {
+    const component = mount(<Landing />);
+    const event = component.find('input[type="text"]').simulate('change', {
+      target: { value: 'Hawaii' }
+    })
+    expect(event.getDOMNode().value).toBe('Hawaii')
+  })
+})
+
+describe('Destination Results', () => {
+  it('Destinations rendered', () => {
+    const component = shallow(<Landing />);
+    expect(component.find('.destinations').exists()).toBe(true);
+  })
+
+  it('Destinations should mount once', function () {
+    expect(mount(<Landing />).find('.destinations').length).toBe(1);
+  });
+
+  it('Destination text', () => {
+    const component = shallow(<Landing />);
+    expect(component.find('[className="destinations"]').text()).toEqual('SEARCHED DESTINATIONS HERE');
+  })
+})
