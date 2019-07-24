e7c926af01087ee5dd0bbe548efb518d94e846d7
azinoVo <azinovo22@gmail.com>
2019-07-24 14:01:07 -0500

    added axios request to BE hitting general / EP

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,18 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
+// Landing is the initial page that users will see when they enter the application
 function Landing() {
+  // User-Input state
   const [destination, setDestination] = useState('');
+  const [searchedDestination, setSearch] = useState('')
 
+  // Sets the state to user-input
   const handleOnChange = (event) => {
     setDestination(event.target.value);
   };
 
-
+  // Performs action when user clicks the ROAM button
   const handleOnSubmit = (event) => {
     event.preventDefault();
     console.log(`I am roaming ${destination}`); // eslint-disable-line
+
+    // Request to backend that will request to API and send back the data?
+    axios.get('http://localhost:8000/', destination)
+      .then((response) => {
+          console.log(response);  // eslint-disable-line
+          setSearch(response.data)
+      })
+      .catch((error) => {
+          console.log(error);  // eslint-disable-line
+      });
   };
 
+  // Check the state
   useEffect(() => {
     console.log(destination); // eslint-disable-line
   });
@@ -36,6 +52,7 @@ function Landing() {
 
       <div className="destinations">
         SEARCHED DESTINATIONS HERE
+        <p>{searchedDestination}</p>
       </div>
     </>
   );
