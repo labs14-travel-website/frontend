347e7f76ad2db61cbfcbb6ced97d98a350fc5a67
josh182014 <josh182014@gmail.com>
2019-07-26 11:04:51 -0600

    refactored populardestinations component

--- src/components/PopularDestinations/PopularDestinations.jsx
+++ src/components/PopularDestinations/PopularDestinations.jsx
@@ -1,38 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const locations = [
-  { city: 'Hong Kong', country: 'China' },
-  { city: 'Bangkok', country: 'Thailand' },
-  { city: 'London', country: 'United Kingdom' },
-  { city: 'Paris', country: 'France' },
-  { city: 'Dubai', country: 'United Arab Emirates' },
-  { city: 'New York City', country: 'United States' },
-  { city: 'Kuala Lumpur', country: 'Malaysia' },
-  { city: 'Shenzhen', country: 'China' },
-  { city: 'Phuket', country: 'Thailand' },
-  { city: 'Istanbul', country: 'Turkey' },
-  { city: 'Delhi', country: 'India' },
-  { city: 'Tokyo', country: 'Japan' },
-  { city: 'Rome', country: 'Italy' },
-  { city: 'Antalya', country: 'Turkey' },
-  { city: 'Taipei', country: 'Taiwan' },
-  { city: 'Guangzhou', country: 'China' },
-  { city: 'Mumbai', country: 'India' },
-  { city: 'Prague', country: 'Czech Republic' },
-];
+const PopularDestinations = () => {
+  const locations = [
+    { city: 'Hong Kong', country: 'China' },
+    { city: 'Bangkok', country: 'Thailand' },
+    { city: 'London', country: 'United Kingdom' },
+    { city: 'Paris', country: 'France' },
+    { city: 'Dubai', country: 'United Arab Emirates' },
+    { city: 'New York City', country: 'United States' },
+    { city: 'Kuala Lumpur', country: 'Malaysia' },
+    { city: 'Shenzhen', country: 'China' },
+    { city: 'Phuket', country: 'Thailand' },
+    { city: 'Istanbul', country: 'Turkey' },
+    { city: 'Delhi', country: 'India' },
+    { city: 'Tokyo', country: 'Japan' },
+    { city: 'Rome', country: 'Italy' },
+    { city: 'Antalya', country: 'Turkey' },
+    { city: 'Taipei', country: 'Taiwan' },
+    { city: 'Guangzhou', country: 'China' },
+    { city: 'Mumbai', country: 'India' },
+    { city: 'Prague', country: 'Czech Republic' },
+  ];
 
-const shuffled = locations.sort(() => 0.5 - Math.random());
+  const shuffled = locations.sort(() => 0.5 - Math.random());
 
-const selectedLocations = shuffled.slice(0, 4);
+  const selectedLocations = shuffled.slice(0, 4);
 
-const PopularDestinations = () => (
-  <div>
-    <h2>Popular Destinations</h2>
-    {selectedLocations.map(location => (
-      <div>{location.city}, {location.country}</div>
-      // <div><CardDisplay location={each} /></div> //TODO pass locations to CardDisplay
-    ))}
-  </div>
-);
+  useEffect(() => {
+    console.log('hi');
+    // make axios call to backend
+
+  });
+
+  return (
+    <div>
+      <h2>Popular Destinations</h2>
+      {selectedLocations.map(location => (
+        <div>{location.city}, {location.country}</div>
+        // <div><CardDisplay location={each} /></div> //TODO pass locations to CardDisplay
+      ))}
+    </div>
+  );
+};
 
 export default PopularDestinations;
