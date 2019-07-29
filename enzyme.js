65fcf9e62081ba3a7dfc6031b7ff3841f36ff988
josh182014 <josh182014@gmail.com>
2019-07-29 14:08:40 -0600

    resolved linting issues

--- enzyme.js
+++ enzyme.js
@@ -1,6 +1,8 @@
-import Enzyme, { configure, shallow, mount, render } from 'enzyme';
+import Enzyme, {
+  configure, shallow, mount, render,
+} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 export { shallow, mount, render };
-export default Enzyme; 
+export default Enzyme;
--- src/components/CardDisplay/CardDisplay.jsx
+++ src/components/CardDisplay/CardDisplay.jsx
@@ -1,24 +1,22 @@
-import React, { Suspense } from "react";
-import Loader from "react-loader-spinner";
-import PropTypes from "prop-types";
+import React, { Suspense } from 'react';
+import Loader from 'react-loader-spinner';
+import PropTypes from 'prop-types';
 
-export const CardDisplay = (props) => {
-  return (
-    <div className="container">
-      
-      <Suspense
-        fallback={
-          <Loader type="Ball-Triangle" color="#00BFFF" height="90" width="60" />
+export const CardDisplay = props => (
+  <div className="container">
+
+    <Suspense
+      fallback={
+        <Loader type="Ball-Triangle" color="#00BFFF" height="90" width="60" />
         }
-      >
-        <h2>{props.location}</h2>
-      </Suspense>
-    </div>
-  );
-};
+    >
+      <h2>{props.location}</h2>
+    </Suspense>
+  </div>
+);
 
- CardDisplay.propTypes = {
-    location: PropTypes.string
+CardDisplay.propTypes = {
+  location: PropTypes.string,
 };
 
 export default CardDisplay;
