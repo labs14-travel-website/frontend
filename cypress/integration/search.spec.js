4ab4eacc327f418c8a7d47544b6c961c5bb5475b
azinoVo <azinovo22@gmail.com>
2019-07-30 12:00:08 -0500

    added lint fixes

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -1,24 +1,21 @@
 context('Destinations Search Bar', () => {
-    beforeEach(() => {
-        cy.visit('/');
-    });
+  beforeEach(() => {
+    cy.visit('/');
+  });
 
-    it('tests true', () => {
-        expect(true).to.equal(true);
-    });
+  it('tests true', () => {
+    expect(true).to.equal(true);
+  });
 
-    it('looks for input field', () => {
-        cy.get('.search').should(
-            'contain',
-            'Roam',
-        );
-    });
+  it('looks for input field', () => {
+    cy.get('.search').should(
+      'contain',
+      'Roam',
+    );
+  });
 
-    it('Type into the input field', () => {
-        cy.get('input[placeholder=Destination]').type('Hawaii').type('{enter}');
-        cy.get('.destinations').should('contain', 'The test server is up and running!');
-    });
+  it('Type into the input field', () => {
+    cy.get('input[placeholder=Destination]').type('Hawaii').type('{enter}');
+    cy.get('.destinations').should('contain', 'The test server is up and running!');
+  });
 });
-
-
-
