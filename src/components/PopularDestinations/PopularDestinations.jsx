c33043dbd393253ab0b218914c285c38526bafe2
josh182014 <josh182014@gmail.com>
2019-07-31 11:20:52 -0600

    added unit tests

--- src/components/PopularDestinations/PopularDestinations.jsx
+++ src/components/PopularDestinations/PopularDestinations.jsx
@@ -40,7 +40,7 @@ const PopularDestinations = () => {
       <h2>Popular Destinations</h2>
       {
         selectedLocations.map(location => (
-          <CardDisplay location={location} handleClick={handleClick} />
+          <CardDisplay className='test' location={location} handleClick={handleClick} />
         ))
       }
     </div>
