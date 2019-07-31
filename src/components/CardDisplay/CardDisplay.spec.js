141e7b70992493ed537f64e26f230afb615e0e92
josh182014 <josh182014@gmail.com>
2019-07-31 14:50:03 -0600

    fixed linting problems

--- src/components/CardDisplay/CardDisplay.spec.js
+++ src/components/CardDisplay/CardDisplay.spec.js
@@ -4,25 +4,24 @@
 // work on tests
 // enzyme??
 // cypress??
-import React from "react";
-import Enzyme, { shallow, mount } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import CardDisplay from ".";
+import React from 'react';
+import Enzyme, { shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import CardDisplay from '.';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 const mockDestinations = [
   {
-    city: "New York",
-    country: "USA"
-  }
+    city: 'New York',
+    country: 'USA',
+  },
 ];
 
-describe("Confirm CardDisplay Mounts", () => {
-  it("renders", () => {
-
+describe('Confirm CardDisplay Mounts', () => {
+  it('renders', () => {
     const wrapper = shallow(
-      <CardDisplay location={mockDestinations} handleClick={() => true} />
+      <CardDisplay location={mockDestinations} handleClick={() => true} />,
     );
 
     expect(wrapper.exists()).toBe(true);
