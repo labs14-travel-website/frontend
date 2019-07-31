48521dea55b5ce3e47c375b5c6bb8647976254f0
Arron M <marshall.arron@gmail.com>
2019-07-31 16:42:21 -0700

    Update tests to account for css modules names

--- src/components/Search/Search.spec.jsx
+++ src/components/Search/Search.spec.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import { shallow, mount } from '../../enzyme';
 import Search from './Search';
-import styles from './Search.module.scss';
 
 describe('Search Bar', () => {
-  it('Search Bar rendered', () => {
-    const component = shallow(<Search />);
-    expect(component.find(styles.Hero__search).exists()).toBe(true);
-  });
-
   it('Search should mount once', () => {
     const component = mount(<Search />);
-    expect(component.find('.search').exists()).toBe(true);
+    expect(component.find('[className^="Hero"]').exists()).toBe(true);
   });
 
   it('Search Bar text', () => {
     const component = shallow(<Search />);
-    expect(component.find('[className="search"]').text()).toEqual('Roam');
+    expect(component.find('[className^="Hero__search_form__submit"]').text()).toEqual('Roam');
   });
 
   it('Search Bar Simulation', () => {
