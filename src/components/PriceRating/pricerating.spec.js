68a4c2fafb9026447ff281ea011bf060e3bfe1ae
azinoVo <azinovo22@gmail.com>
2019-08-06 13:02:47 -0500

    added unit tests for rating and pricerating

--- src/components/PriceRating/pricerating.spec.js
+++ src/components/PriceRating/pricerating.spec.js
@@ -1,3 +1,42 @@
-it('Should 1 be 1', () => {
-  expect(1).toBe(1);
+import React from 'react';
+import { mount } from '../../enzyme';
+import PriceRating from './PriceRating';
+
+// Naming conventions examples as used by the star rating dependency 
+// http://voronianski.github.io/react-star-rating-component/example/
+
+const mountComp = (props = {}) => {
+  const component = mount(<PriceRating {...props} />);
+  return component;
+};
+
+describe('PriceRating Component', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    const props = {
+      price: 3
+    };
+    wrapper = mountComp(props);
+  });
+
+  it('Props passed into component should be 3', () => {
+    expect(wrapper.props().price).toBe(3);
+  });
+
+  it('Should mount one Star Component', () => {
+    const component = wrapper.find('StarRatingComponent')
+    expect(component.length).toBe(1);
+  });
+
+  it('Star Component should have a maximum value of 4', () => {
+    const component = wrapper.find('.dv-star-rating-star')
+    expect(component.length).toBe(4);
+  });
+
+  it('User should see 3 green or "full" dollar signs', () => {
+    const component = wrapper.find('.dv-star-rating-full-star')
+    expect(component.length).toBe(3);
+  });
+
 });
