08fd5ecfa02c8dce2f6dc41553147f6c6ee786a4
azinoVo <azinovo22@gmail.com>
2019-08-16 17:25:13 -0500

    added test for profile

--- src/views/Profile/Profile.spec.js
+++ src/views/Profile/Profile.spec.js
@@ -1,3 +1,31 @@
-it('should 1 be 1', () => {
-  expect(1).toBe(1);
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Profile from './Profile';
+import { mount } from '../../enzyme';
+
+describe('Profile Component', () => {
+  it('Component should mount', () => {
+    const props = {
+      user: {
+        name: 'Ryan who?',
+        email: '123@gmail.com',
+        googleId: '12345',
+      },
+    };
+    const wrapper = mount(<Router><Profile {...props} /></Router>);
+    expect(wrapper).toBeDefined();
+  });
+
+  it('Username should display', () => {
+    const props = {
+      user: {
+        name: 'Ryan who?',
+        email: '123@gmail.com',
+        googleId: '12345',
+      },
+    };
+    const wrapper = mount(<Router><Profile {...props} /></Router>);
+    const headOne = wrapper.find('h1').first();
+    expect(headOne.text()).toBe('Ryan who?');
+  });
 });
