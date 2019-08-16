98e4fd6af1358b09e59d8f1427d8bfcd28ef9841
azinoVo <azinovo22@gmail.com>
2019-08-16 17:01:22 -0500

    pulled from master writing test

--- src/views/Profile/Profile.spec.js
+++ src/views/Profile/Profile.spec.js
@@ -1,3 +1,23 @@
-it('should 1 be 1', () => {
-  expect(1).toBe(1);
+import React from 'react';
+import Profile from './Profile';
+import { mount } from '../../enzyme';
+import { BrowserRouter as Router } from 'react-router-dom'
+
+describe('Profile Component', () => {
+  it('Component should mount', () => {
+    const props = {
+      user: "Ryan who?"
+    };
+    const wrapper = mount(<Router><Profile {...props} /></Router>);
+    expect(wrapper).toBeDefined();
+  });
+
+  it('', () => {
+    
+  });
+
+  it('', () => {
+   
+  });
 });
+
