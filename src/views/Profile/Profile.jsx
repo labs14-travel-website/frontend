07041707e1e3fd7c6c2401d775ab9daaffbd8631
Arron M <marshall.arron@gmail.com>
2019-08-16 13:08:34 -0700

    Show user name from google profile account response

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import { Redirect } from 'react-router';
+import PropTypes from 'prop-types';
 
-function Profile() {
-  return (
-    <div>
-      <h1>Nguyen Vo</h1>
-      <h2>Favorites</h2>
-    </div>
-  );
-}
+const Profile = ({ user }) => {
+  console.log('abc'); // eslint-disable-line
+  if (user.name) {
+    return (
+      <div>
+        <h1>{user.name}</h1>
+        <h2>Favorites</h2>
+      </div>
+    );
+  }
+
+  return <Redirect to="/" />;
+};
+
+Profile.propTypes = {
+  user: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    googleId: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Profile;
