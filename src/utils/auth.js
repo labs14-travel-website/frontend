c7bf8e11863913743ac0d71ce1f38c3d464bf88f
Arron M <marshall.arron@gmail.com>
2019-08-24 12:19:16 -0700

    Abstract login function

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,8 +1,45 @@
+import axios from 'axios';
+import decode from 'jwt-decode';
+import store from './jwt-store';
 
+const setHeader = (token) => {
+  // set authorization header for all future axios requests
+  axios.defaults.headers.common.authorization = token;
+};
 
-const login = '';
-const logout = '';
-const fail = '';
+const logout = () => {
+  // Remove user token, push logout to google
+  // return empty user object
+  return {};
+};
+
+const login = async ({ tokenId: token }) => {
+  try {
+    setHeader(token);
+    const { data: { message: authorization } } = await axios
+      .post(`${process.env.REACT_APP_ENDPOINT}/api/auth`);
+
+    if (authorization !== 'success auth') {
+      throw new Error({ message: 'Unsuccessful Authentication' });
+    }
+
+    store.add(token);
+    const { name, email, sub: googleId } = decode(token);
+    return {
+      name,
+      email,
+      googleId,
+    };
+  } catch (error) {
+    return logout();
+  }
+};
+
+const fail = (res) => {
+  // Log failed auth, show potential message
+  // return empty user object
+  return {};
+};
 
 module.exports = {
   login,
